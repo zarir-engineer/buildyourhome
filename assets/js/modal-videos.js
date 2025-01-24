4ada91fe0737b3943de7ed4ec6e9bf1802d391aa
Nishith Singhai <singhai.nish@gmail.com>
2025-01-24 12:50:47 +0530

    update

--- assets/js/modal-videos.js
+++ assets/js/modal-videos.js
@@ -1,27 +1,27 @@
 // Open modal functionality
-document.querySelectorAll('.open-modal').forEach((button) => {
-  button.addEventListener('click', (event) => {
-    event.preventDefault(); // Prevent default link behavior
-    const modalId = button.getAttribute('data-modal');
-    const modal = document.getElementById(modalId);
-    modal.style.display = 'block';
-  });
-});
+//document.querySelectorAll('.open-modal').forEach((button) => {
+//  button.addEventListener('click', (event) => {
+//    event.preventDefault(); // Prevent default link behavior
+//    const modalId = button.getAttribute('data-modal');
+//    const modal = document.getElementById(modalId);
+//    modal.style.display = 'block';
+//  });
+//});
 
 // Close modal functionality
-document.querySelectorAll('.close').forEach((button) => {
-  button.addEventListener('click', () => {
-    const modalId = button.getAttribute('data-modal');
-    const modal = document.getElementById(modalId);
-    modal.style.display = 'none';
-  });
-});
+//document.querySelectorAll('.close').forEach((button) => {
+//  button.addEventListener('click', () => {
+//    const modalId = button.getAttribute('data-modal');
+//    const modal = document.getElementById(modalId);
+//    modal.style.display = 'none';
+//  });
+//});
 
 // Close modal when clicking outside of it
-window.addEventListener('click', (event) => {
-  document.querySelectorAll('.modal').forEach((modal) => {
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    }
-  });
-});
+//window.addEventListener('click', (event) => {
+//  document.querySelectorAll('.modal').forEach((modal) => {
+//    if (event.target === modal) {
+//      modal.style.display = 'none';
+//    }
+//  });
+//});
